dc17d14ed453eb03a06dfcab6d89af0ec6b14026
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-05-29 13:12:08 -0400

    Setup test environment

---
+++ src/test/java/com/mikedeejay2/oosql/OOSQLTest.java
@@ -0,0 +1,13 @@
+package com.mikedeejay2.oosql;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OOSQLTest
+{
+    @Test
+    public void test()
+    {
+    }
+}
